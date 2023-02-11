d838930f745372004fbc7cf0a76167acb8d44837
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-02-11 17:33:02 +0530

    Initial commit

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -14,23 +14,29 @@ function index() {
     return (
         <>
 
-            <div className='main-container flex items-center justify-center w-full h-auto'>
-                <div className='main-wrapper flex items-center justify-center flex-col w-full h-full m-5 md:m-10 gap-5 md:gap-10'>
-                    <div className='banner w-full h-full flex items-end justify-start'>
-                        {/* <img className='absolute rocket mb-3' src={rocket} alt='' /> */}
-                        <div className='w-full h-full flex items-center justify-center'>
-                            <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='flex justify-center mx-auto'>
-                                <img src={banner} alt='banner' className='banner-image duration-300 w-10/12 transition-all' />
+            <div className='flex items-center justify-center w-full h-auto'>
+                <div className='flex items-center justify-center flex-col w-full h-full m-5 md:m-10 gap-5 md:gap-4'>
+                    {/* <div className='banner w-full h-full flex items-end justify-start'>
+                        <div className='flex flex-col space-y-6'>
+                            <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='flex justify-center w-10/12 mx-auto'>
+                                <img src={banner} alt="merch" />
                             </motion.div>
-
                         </div>
+
+                    </div> */}
+                    <div className='flex flex-col space-y-6 h-full w-11/12'>
+
+                        <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className='flex justify-center mx-auto'>
+                            <img src={banner} alt="merch" />
+                        </motion.div>
                     </div>
+
 
 
                     <div className='w-full flex items-center justify-center'>
                         <Filters />
                     </div>
-                    
+
 
                     <div className='w-full h-full flex-wrap gap-12 flex items-center justify-center'>
                         <Cards />
